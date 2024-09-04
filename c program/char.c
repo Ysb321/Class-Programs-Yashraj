b7004685f447358bd24b82dcea32b80ed801c5eb
ysb321 <ysb1571@gmail.com>
2024-09-04 19:51:24 +0530

    cpp

---
+++ c program/char.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    char n[20] = "Yashraj";
+    printf(" %u", n);
+    printf(" %u", &n[0]);
+}
---
+++ c++ programs/first.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Person{
+    // class in by default private 
+    int age;
+    char name[20];
+    char gender;
+
+    public:
+
+    void setData(int a, char *n, char g) // we can write char n[20]
+    {
+        age =a;
+        gender = g;
+        strcpy(name, n);
+    }
+    void display()
+    {
+        cout<<"Name: "<<name<<"\n";
+        cout<<"Age: "<<age<<endl;
+        cout<<"Gender: "<<gender<<endl;
+    }
+};
+ 
+ int main(){
+    Person P;
+    // P.age = 20;
+    P.setData(20, "ABC", 'M');
+    P.display();
+    return 0;
+ }
