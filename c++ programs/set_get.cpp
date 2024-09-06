659a50c95127fe9fa22a691fb8d11a9541f2a2d3
ysb321 <ysb1571@gmail.com>
2024-09-06 20:51:53 +0530

    cpp

---
+++ c++ programs/set_get.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Person{
+    int age;
+    char name[20];
+    char gender;
+
+    public:
+
+    void setData(int age, char *name, char gender)
+    {
+        this -> age = age;
+        strcpy(this -> name, name);
+        this -> gender = gender;
+    }
+
+    void display()
+    {
+        cout << "Age: "<< age << endl;
+        cout << "Name: "<< name << endl;
+        cout << "Gender: "<< gender << endl << endl;
+    }
+
+    void setAge(int age){
+        this -> age = age;
+    }
+    void setName(char *name){
+        strcpy(this -> name, name);
+    }
+    void setGender(char gender){
+        this -> gender = gender;
+    }
+
+    int getAge()
+    {
+        return age;
+    }
+
+    char *getName()
+    {
+        return name;
+    }
+
+    char getGender()
+    {
+        return gender;
+    }
+    
+};
+
+int main(){
+    Person P;
+    P.setData(20, "Yashraj", 'M');
+    P.display();
+    P.setAge(22);
+    P.display();
+}
