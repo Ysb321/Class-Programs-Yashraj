a6bdbbd9ffa4eb370be498fe212a89ca8390ba47
ysb321 <ysb1571@gmail.com>
2024-09-22 16:35:23 +0530

    cpp

--- c++ programs/even_num_odd_num.cpp
+++ c++ programs/even_num_odd_num.cpp
@@ -36,10 +36,47 @@ class even_num
     }
 
 };
+
+class odd_num
+{
+    int* i;
+    int* num;
+
+    public:
+    odd_num()
+    {
+       num = new int(12);
+    }
+    odd_num(int i, int num)
+    {
+        this -> i = new int(i);
+        this -> num = new int(num);
+        cout<<"Enter a Number upto getting odd number: "<< num <<endl;
+        for(; i<num; i++)
+        {
+            if(i % 2 !=0)
+            cout << endl << i;
+        }
+    }
+    ~odd_num()
+    {
+        delete num;
+        delete i;
+    }
+
+    void display()
+    {
+        cout <<endl;
+    }
+};
+
 int main()
 {
-    even_num E;
-    even_num E1(0, 21);
-    E.display();
-    E1.display();
+    even_num *ptr = new even_num(1, 21);
+    ptr -> display();
+    cout<<"+++++++"<<endl;
+    odd_num *ptr2 = new odd_num(1, 21);
+    ptr2 -> display();
+    delete ptr;
+    delete ptr2;
 }
