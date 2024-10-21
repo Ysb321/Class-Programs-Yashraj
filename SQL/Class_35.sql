421e5545459fc2f6260517d69a31887414109ecc
ysb321 <ysb1571@gmail.com>
2024-10-21 18:48:49 +0530

    SQL

--- SQL/Class_35.sql
+++ SQL/Class_35.sql
@@ -2,4 +2,6 @@ update employee set First_name = '_prathmesh' where employees_id = 109;
 
 select employee_is, salary ,COMMISSION_PCT, (salary+(salary*COMMISSION_PCT)) as Total from employees;
 
-SQL> select employee_id, salary, commission_pct, nvl(commission_pct, 0), salary+salary*nvl(commission_pct, 0) as Total from employees; -- nvl will check where is null vaue in commision_pct then change it to 0
+select * from employees where first_name like '\_%sh' escape '\';
+
+select employee_id, salary, commission_pct, nvl(commission_pct, 0), salary+salary*nvl(commission_pct, 0) as Total from employees; -- nvl will check where is null vaue in commision_pct then change it to 0
