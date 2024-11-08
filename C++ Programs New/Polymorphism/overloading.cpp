e9795ad28027d436c49813f3d7ede97c1d0f869d
ysb321 <ysb1571@gmail.com>
2024-11-08 18:33:37 +0530

    Polymorphism in cpp

---
+++ C++ Programs New/Polymorphism/overloading.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+// method or function overloading
+class A{
+    protected:
+    int a;
+
+    public:
+    A(){
+        a =0;
+    }
+    A(int a){
+        this -> a = a;
+    }
+
+    void display() // zero argument
+    {
+        cout<<"A = "<<a<<endl;
+    }
+};
+
+class B: public A{
+    private: 
+    int b;
+
+    public:
+    B(){
+        b =0;
+    }
+    B(int a, int b): A(a){
+        this -> b = b;
+    }
+
+    void display(int prefix) // one argument 
+    {
+        cout<<prefix<<" "<<a<<endl;
+    }
+};
+
+int main()
+{
+
+    B obj4(10,30);
+    obj4.display(12);
+    obj4.A::display();
+}
+
+
