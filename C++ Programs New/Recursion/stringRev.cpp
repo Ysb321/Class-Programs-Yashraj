4ebe38fddd714cc4a7457c5704c7fd0d788e82c6
ysb321 <ysb1571@gmail.com>
2024-11-14 19:55:27 +0530

    recursion in cpp

--- C++ Programs New/Recursion/stringRev.cpp
+++ C++ Programs New/Recursion/stringRev.cpp
@@ -3,29 +3,36 @@
 using namespace std;
 
 class StringRev {
-public:
-    char* stringReverse(char* str, int start, int end) {
+
+
+    public:
+    char* stringReverse(char* str) {
+        int len = strlen(str);
+
+        return stringRev(str, 0, len- 1);
+    }
+
+    char* stringRev(char* str, int start, int end)
+    {
         if (start >= end) {
             return str;
         }
         char temp = str[start];
         str[start] = str[end];
         str[end] = temp;
-
-
-        return stringReverse(str, start + 1, end - 1);
+        return stringRev(str, start+1, end-1);
     }
+    
 };
 
 int main() {
     char str1[50];  
-    int len = strlen(str1);
 
     cout << "Enter a String: ";
     gets(str1);  
     
     StringRev rev;
-    char* result = rev.stringReverse(str1, 0, len - 1);
+    char* result = rev.stringReverse(str1);
 
     cout << "String Reverse is " << result << endl;
 
