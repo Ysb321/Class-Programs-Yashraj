17e83bd22feb8a046b75f9cc9c7f65cc7cc51b85
ysb321 <ysb1571@gmail.com>
2024-11-16 21:56:46 +0530

    DSA in cpp

--- C++ Programs New/DSA/Linked_list.cpp
+++ C++ Programs New/DSA/Linked_list.cpp
@@ -1,5 +1,4 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
 using namespace std;
 
 class Node
@@ -7,21 +6,21 @@ class Node
     Node* next;
     int data;
 
-    public:
-
+public:
     Node(int data)
     {
-        this ->data = data;
+        this->data = data;
+        next = NULL;
     }
 
     void setData(int data)
     {
-        this -> data= data;
+        this->data = data;
     }
 
     void setNext(Node* next)
     {
-        this -> next = next;
+        this->next = next;
     }
 
     Node* getNext()
@@ -39,77 +38,95 @@ class LinkedList
 {
     Node* head;
 
-    public:
-
-    LinkedList(){
+public:
+    LinkedList()
+    {
         head = NULL;
     }
 
     void addStart(int data)
     {
-        Node* temp;
-        temp = new Node(data);
+        Node* temp = new Node(data);
         temp->setNext(head);
         head = temp;
     }
 
-    void addEnd(int data){
+    void addEnd(int data)
+    {
         Node* temp = new Node(data);
-        if(head==NULL)
+        if (head == NULL)
         {
             head = temp;
-        }else{
+        }
+        else
+        {
             Node* trav = head;
-            while(trav->getNext()!=NULL){
+            while (trav->getNext() != NULL)
+            {
                 trav = trav->getNext();
-            } 
-             trav->setNext(temp);
+            }
+            trav->setNext(temp);
         }
     }
 
     void deleteEnd()
     {
-        if(head==NULL)
+        if (head == NULL)
         {
-            cout<<"Linked List is Empty";
-        }else{
-            Node* prev;
-            Node* pres;
-
-            prev = head;
-            pres = head;
-
-            while(pres -> getNext()!=NULL)
+            cout << "Linked List is Empty" << endl;
+        }
+        else
+        {
+            if (head->getNext() == NULL)
             {
-                prev = pres;
-                pres = pres -> getNext();
+                delete head;
+                head = NULL;
             }
+            else
+            {
+                Node* prev = head;
+                Node* pres = head->getNext();
+
+                while (pres->getNext() != NULL)
+                {
+                    prev = pres;
+                    pres = pres->getNext();
+                }
                 prev->setNext(NULL);
                 delete pres;
+            }
         }
     }
 
     void deleteStart()
     {
-        if(head==NULL)
+        if (head == NULL)
         {
-            cout<<"Linked List is Emty";
-        }else{
+            cout << "Linked List is Empty" << endl;
+        }
+        else
+        {
             Node* temp = head;
-            head = head-> getNext();
-            delete temp; 
+            head = head->getNext();
+            delete temp;
         }
     }
 
     void traverse()
     {
-        Node* trav = head;
-        while(trav->getNext()!=NULL)
+        if (head == NULL)
         {
-            cout<<trav->getData()<<endl;
-            trav = trav->getNext();
-        }     
-            cout<<trav->getData()<<endl;
+            cout << "Linked List is already empty" << endl;
+        }
+        else
+        {
+            Node* trav = head;
+            while (trav != NULL)
+            {
+                cout << trav->getData() << endl;
+                trav = trav->getNext();
+            }
+        }
     }
 };
 
@@ -119,7 +136,13 @@ int main()
     obj.addStart(23);
     obj.addStart(25);
     obj.addStart(26);
-    obj.addStart(26);
+    obj.deleteEnd();
+    obj.addStart(29);
+    obj.addEnd(22);
+    obj.addEnd(24);
+    obj.deleteStart();
     obj.addStart(27);
     obj.traverse();
+
+    return 0;
 }
