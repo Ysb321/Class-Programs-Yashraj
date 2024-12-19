7fd3cc81d1d2c1aeb35f26a0e727a4d87eb7212a
ysb321 <ysb1571@gmail.com>
2024-12-19 18:56:47 +0530

    SQL

---
+++ SQL/Class_67.sql
@@ -0,0 +1,11 @@
+
+/*
+PL/SQL
+
+----- Important
+7, 19, 24, 25,
+
+exception, procedure, trigger, PROCEDURE TRIGGER DIFFERENCE, nodatafound sql%nodatafound %datanotfound DIFFERENCE
+
+---- NOt REQUIRED to Study
+Q8, Q9, Q10, Q14, 15 ,16, 22,
